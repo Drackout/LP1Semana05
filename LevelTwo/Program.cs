76eac65811ee3613814a105c95c1f1002a0704d1
Drackout <mikatx2@hotmail.com>
2025-03-20 22:52:32 +0000

    Create Main and Function to show image

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Spectre.Console;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                ShowImg(args[0], char.Parse(args[1]));
+            }
+            else
+            {
+                ShowImg("tux.jpg", Convert.ToChar(24));
+            }
+        }
+
+        private static void ShowImg(string imgName, char size)
+        {
+            CanvasImage img = new CanvasImage(imgName);
+            img.MaxWidth(size);
+            AnsiConsole.Write(img);
+        }
+    }
+}
