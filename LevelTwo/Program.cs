7fb7570140c81c87e855391276178bf9a53035bf
Drackout <mikatx2@hotmail.com>
2025-03-20 23:12:12 +0000

    Changes on img files, use numbers as int instead of chars

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -8,16 +8,12 @@ namespace LevelTwo
         private static void Main(string[] args)
         {
             if (args.Length == 2)
-            {
-                ShowImg(args[0], char.Parse(args[1]));
-            }
+                ShowImg(args[0], int.Parse(args[1]));
             else
-            {
-                ShowImg("tux.jpg", Convert.ToChar(24));
-            }
+                ShowImg("tux.jpg", 24);
         }
 
-        private static void ShowImg(string imgName, char size)
+        private static void ShowImg(string imgName, int size)
         {
             CanvasImage img = new CanvasImage(imgName);
             img.MaxWidth(size);
