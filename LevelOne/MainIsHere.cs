65c084efb8f4715805756d3e3261fb5b58c3b35b
Drackout <mikatx2@hotmail.com>
2025-03-20 22:26:40 +0000

    try to do auto-merge conflict Again

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -6,10 +6,10 @@ namespace LevelOne
     {
         private static void Main(string[] args)
         {
-            //
+            // O Automatic merge failed; do stor no PDF não funciona
+            //Segui tudo passo a passo e fez merge como se nao fosse nada
             Console.WriteLine("Hello LP1!");
-            Console.WriteLine("Nice day");
-            Console.WriteLine("Bye!");
+
         }
     }
 }
